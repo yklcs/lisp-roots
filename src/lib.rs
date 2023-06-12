2133a5a7d252d8ed222f21d562bed517ea5f7610
Lucas Yunkyu Lee <lee@yklcs.com>
2023-06-12 12:46:51 +0000

    add wasm

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,19 @@ pub mod expr;
 mod list;
 mod primitives;
 pub mod read;
+
+pub fn read_eval(src: String) -> String {
+    let xs = read::read(src).unwrap();
+    let mut env = env::Env::new_global();
+
+    let mut output = String::new();
+
+    for x in xs {
+        let result = x.eval(&mut env).unwrap();
+        if result != expr::Expr::new_nil() {
+            output += &format!("{}\n", result);
+        }
+    }
+
+    output
+}
---
+++ wasm/src/lib.rs
@@ -0,0 +1,24 @@
+use lisp_roots::read_eval;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn lisp_root_read_eval(src: &str) -> String {
+    read_eval(src.to_string())
+}
+
+#[wasm_bindgen(start)]
+fn main() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_inner_html("Hello from Rust!");
+
+    body.append_child(&val)?;
+
+    Ok(())
+}
